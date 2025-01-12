88a430297c2bfbbd0ba37605168c2f30ecdf5bee
gungunjain36 <jgungun1723@gmail.com>
2025-01-12 17:45:25 +0530

    update main.rs

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\projects_rs\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\projects_rs\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\projects_rs\variables\target\debug\variables.exe: C:\projects_rs\variables\src\main.rs
